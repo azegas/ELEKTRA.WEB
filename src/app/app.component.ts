a7198b72430957378640607693b6fe26e2ada84f
Arvydas <azegaspa@gmail.com>
2025-02-18 17:10:41 +0200

    fetch from backend
    dont forget in the environments set this up for cors:
    
    export const environment = {
      apiUrl: 'https://localhost:7081',
    };

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherforecastService } from './weatherforecast.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ELEKTRA.WEB';
+  weatherForecasts: any[] = [];
+
+  weatherForecastService = inject(WeatherforecastService);
+
+  constructor() {
+    this.weatherForecastService.get().subscribe(weatherForecasts => {
+      // assigning a value from the web api to the property weatherForecasts that we have above
+      // and that property can be used in the html file of this component
+      this.weatherForecasts = weatherForecasts;
+    });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    // allowing to use HTTP client throughout the application
+    provideHttpClient(withFetch())
+  ]
 };
