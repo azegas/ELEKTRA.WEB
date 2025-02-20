422b70252499e50366bd95102ebbf4b2ee7feaf8
Arvydas <azegaspa@gmail.com>
2025-02-20 07:25:03 +0200

    random styling fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { WeatherforecastService } from './services/weatherforecast.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
