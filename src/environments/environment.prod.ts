37a420e6636977d38437c20ac01bcd3e5b4e4434
Arvydas <azegaspa@gmail.com>
2025-02-20 06:28:30 +0200

    manage different environments
    so in api service we do import { environment } from '../../environments/environment';
    
    and that really does NOTHING, since when launching or building the project we will always add --configuration <environment> flag which will reflect what we have specified in angular.json
    
    and in there it knows when to build/what

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  apiUrl:
+    'https://elektraapi-hrbtenegexfkhjcs.polandcentral-01.azurewebsites.net',
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: 'https://victorious-pebble-0c9f2d103.4.azurestaticapps.net',
+  apiUrl: '...thisWontBeUsedBecauseItIsOverwrittenByTheEnvironmentSpecificFileInAngulardotJsonSimplyWhenServingAppWriteAflagdashdashConfigurationProductionOrDashDashConfigurationDevelopmentToUseTheEnvironmentSpecificFile...',
 };
