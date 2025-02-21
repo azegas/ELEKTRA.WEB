59dafb138b5a700f079d629f487cc1520d5aa468
Arvydas <azegaspa@gmail.com>
2025-02-21 11:13:25 +0200

    create calculations service, connect to the calculations api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,37 @@
 import { Component, inject } from '@angular/core';
 import { WeatherforecastService } from './services/weatherforecast.service';
+import { CalculationstService } from './services/calculations.service';
 import { environment } from '../environments/environment';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
+  imports: [
+    CommonModule // purely for json pipe in template (probably has more stuff in it)
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ELEKTRA.WEB';
+
+  // TODO add interfaces/types instead of any
   weatherForecasts: any[] = [];
+  calculations: any[] = [];
 
   weatherForecastService = inject(WeatherforecastService);
+  calculationstService = inject(CalculationstService);
 
   constructor() {
     console.log('ENV:', environment.env);
 
-    this.weatherForecastService.get().subscribe(weatherForecasts => {
+    this.weatherForecastService.get().subscribe((weatherForecasts) => {
       // assigning a value from the web api to the property weatherForecasts that we have above
       // and that property can be used in the html file of this component
       this.weatherForecasts = weatherForecasts;
+    });
+
+    this.calculationstService.get().subscribe((calculations) => {
+      this.calculations = calculations;
     });
   }
 }
