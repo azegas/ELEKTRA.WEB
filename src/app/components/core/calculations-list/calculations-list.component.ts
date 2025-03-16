7c3eab2568471517206e74597419a990f7c41fba
Arvydas <azegaspa@gmail.com>
2025-03-16 15:10:06 +0200

    implement loading spinner for calculation list component

--- src/app/components/core/calculations-list/calculations-list.component.ts
+++ src/app/components/core/calculations-list/calculations-list.component.ts
@@ -2,19 +2,23 @@ import { Component } from '@angular/core';
 import { CalculationCardComponent } from '../calculation-card/calculation-card.component';
 import { CalculationsService } from '../../../services/calculations.service';
 import { Calculation } from '../../../models/calculation';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+
 
 @Component({
   selector: 'app-calculations-list',
-  imports: [CalculationCardComponent],
+  imports: [CalculationCardComponent, ProgressSpinnerModule],
   templateUrl: './calculations-list.component.html',
   styleUrl: './calculations-list.component.css',
 })
 export class CalculationsListComponent {
   calculations: Calculation[] = [];
+  loading = true;
 
   constructor(private calculationService: CalculationsService) {
     this.calculationService.get().subscribe((data) => {
       this.calculations = data;
+      this.loading = false;
     });
   }
 }
